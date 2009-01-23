5134b3ffedd55bd11ff22dd70ed09f515521e953
Chris Rankin <rankin.devon@gmail.com>
2009-01-22 21:50:12 -0500

    updated view helpers to allow variables and html options

--- lib/wizard/application_helper.rb
+++ lib/wizard/application_helper.rb
@@ -13,12 +13,16 @@ module Wizard #:nodoc
       session[:next_wizard_step].to_s
     end
     
-    def link_to_next_step(wizard_name, next_step_name=:next)
-      link_to next_step_name.to_s.humanize, :controller => wizard_name.to_s.camelize.downcase, :action => session[:next_wizard_step].to_s
+    def link_to_next_step(wizard_name, next_step_name=:next, options={}, html_options=nil)
+      options.merge!(:controller => wizard_name.to_s.camelize.downcase, 
+                     :action => session[:next_wizard_step].to_s)
+      link_to(next_step_name.to_s.humanize, options, html_options)
     end
     
-    def link_to_previous_step(wizard_name, previous_step_name=:previous)
-      link_to previous_step_name.to_s.humanize, :controller => wizard_name.to_s.camelize.downcase, :action => session[:previous_wizard_step].to_s
+    def link_to_previous_step(wizard_name, previous_step_name=:previous, options={}, html_options=nil)
+      options.merge!(:controller => wizard_name.to_s.camelize.downcase,
+                     :action => session[:previous_wizard_step].to_s)
+      link_to(previous_step_name.to_s.humanize, options, html_options)
     end
     
   end
